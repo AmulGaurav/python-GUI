c21134ecc48e487f4f6be015ff3d2b69aa897486
Amul Gaurav <amulgaurav003@gmail.com>
2023-04-03 11:22:34 +0530

    Modify code in snake-game and change the file name from data.txt to high_score.txt

--- snake-game/main.py
+++ snake-game/main.py
@@ -6,8 +6,8 @@ import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.title("My snake game")
 screen.bgcolor("black")
+screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
@@ -17,8 +17,8 @@ scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(key="Up", fun=snake.up)
 screen.onkey(key="Down", fun=snake.down)
-screen.onkey(key="Right", fun=snake.right)
 screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
 
 game_is_on = True
 
@@ -27,21 +27,21 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    # Detect collision with food.
+    # Detect collision with the food.
     if snake.head.distance(food) < 15:
-        food.refresh()
         snake.extend()
-        scoreboard.increase_scoreboard()
+        food.refresh()
+        scoreboard.increase_score()
 
     # Detect collision with wall.
-    if (snake.head.xcor() > 280) or (snake.head.xcor() < -280) or (snake.head.ycor() > 280) or (snake.head.ycor() < -280):
-        scoreboard.reset()
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -290:
         game_is_on = False
+        scoreboard.game_over()
 
     # Detect collision with tail.
-    for segment in snake.segments[1:]:
+    for segment in snake.all_segments[1:]:
         if snake.head.distance(segment) < 10:
-            scoreboard.reset()
             game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -1,39 +1,41 @@
 from turtle import Turtle
 
-ALIGNMENT = "center"
-FONT = ("Courier", 13, "normal")
+ALIGNMENT = "left"
+FONT = ("Courier", 16, "normal")
+
 
 class Scoreboard(Turtle):
-
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__()
-        with open("C:\\Users\\admin\\Desktop\\Python Projects\\snake-game\\data.txt") as data:
-            self.high_score = int(data.read())
         self.score = 0
-        self.penup()
+        try:
+            with open(file="snake-game/high_score.txt") as file:
+                pass
+        except:
+            with open(file="snake-game/high_score.txt", mode="w") as file:
+                file.write("0")
+        finally:
+            with open(file="snake-game/high_score.txt") as file:
+                self.high_score = int(file.read())
         self.hideturtle()
         self.color("white")
-        self.goto(0, 270)
+        self.penup()
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        self.goto(x=-290, y=245)
         self.clear()
-        self.write(arg=f"Score: {self.score} | High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+        self.write(
+            f"Score: {self.score}\nHigh Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def increase_scoreboard(self):
+    def increase_score(self):
         self.score += 1
         self.update_scoreboard()
 
-    def reset(self):
-        if self.score > self.high_score:
-            # self.high_score = self.score
-            with open("C:\\Users\\admin\\Desktop\\Python Projects\\snake-game\\data.txt", mode="w") as data:
-                data.write(f"{self.score}")
-            self.goto(0, -10)
-            self.write(arg=f"New High Score: {self.score}", align=ALIGNMENT, font=FONT)    
-        self.game_over()
-        
-
     def game_over(self):
-        self.goto(0, 0)
-        self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
+        if self.score > self.high_score:
+            with open(file="snake-game/high_score.txt", mode="w") as file:
+                file.write(str(self.score))
+        self.score = 0
+        self.goto(x=0, y=0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
